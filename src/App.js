37271e7bf4b3c7f6cb3f00052e0ac92e74138ea5
João Alexandre <j.sa@ufu.br>
2024-08-14 11:57:11 -0300

    v2.0 comentado

--- src/App.js
+++ src/App.js
@@ -11,27 +11,51 @@ function App() {
   const handleButtonClick = (value) => {
     setDisplayValue(displayValue + value);
   };
+  const parseNumber = (num) => {
+    return num.includes('.') ? parseFloat(num) : parseInt(num);
+  };
 
   const handleOperation = (operation) => {
+    // Converte o valor atual do display para um número, seja inteiro ou de ponto flutuante.
     const parsedValue = parseNumber(displayValue);
+
+    // Verifica se já há um resultado anterior e se a operação atual é a mesma da operação anterior.
     if (resultado !== '' && operacao === operation) {
+      // Se houver um resultado e a operação for a mesma, realiza a operação com o resultado anterior e o novo valor.
       setResultado(operate(resultado, parsedValue, operation));
     } else if (resultado === '') {
+      // Se não houver resultado anterior, define o resultado atual como o novo valor convertido.
       setResultado(parsedValue);
     }
+
+    // Limpa o display, pois o usuário irá inserir um novo valor após a operação.
     setDisplayValue('');
+
+    // Atualiza a operação atual para a nova operação selecionada.
     setOperacao(operation);
   };
 
+
   const handleEqual = () => {
+    // Verifica se há algum valor no display antes de tentar calcular o resultado.
     if (displayValue) {
+      // Converte o valor atual do display para um número, seja inteiro ou de ponto flutuante.
       const parsedValue = parseNumber(displayValue);
+
+      // Realiza a operação matemática com o resultado armazenado, o novo valor e a operação selecionada.
       const newResultado = operate(resultado, parsedValue, operacao);
+
+      // Atualiza o display com o novo resultado convertido para string.
       setDisplayValue(newResultado.toString());
+
+      // Atualiza o estado do resultado com o novo valor calculado.
       setResultado(newResultado);
+
+      // Reseta a operação, pois o cálculo foi concluído.
       setOperacao(null);
     }
   };
+
 
   const handleDelete = () => {
     setDisplayValue(displayValue.slice(0, -1));
@@ -43,9 +67,7 @@ function App() {
     setOperacao(null);
   };
 
-  const parseNumber = (num) => {
-    return num.includes('.') ? parseFloat(num) : parseInt(num);
-  };
+
 
   const operate = (num1, num2, operation) => {
     switch (operation) {
